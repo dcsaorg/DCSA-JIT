f097c5e1cbad654da2de6e92daffaed93fb37b21
Niels Thykier <nt@asseco.dk>
2021-07-21 15:55:41 +0200

    Move the receivePayload logic into the service
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ovs/service/NotificationEndpointService.java
+++ src/main/java/org/dcsa/ovs/service/NotificationEndpointService.java
@@ -2,9 +2,13 @@ package org.dcsa.ovs.service;
 
 import org.dcsa.core.service.ExtendedBaseService;
 import org.dcsa.ovs.model.NotificationEndpoint;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface NotificationEndpointService extends ExtendedBaseService<NotificationEndpoint, UUID> {
 
+    Mono<Void> receiveNotification(ServerHttpRequest request, UUID endpointID);
 }
