b152ed31b73b885abbca896ce68aaf87c913eb97
Martin Petersson <mp@asseco.dk>
2021-08-09 09:27:59 +0200

    Update constraints on Timestamp

--- src/main/java/org/dcsa/ovs/model/Timestamp.java
+++ src/main/java/org/dcsa/ovs/model/Timestamp.java
@@ -7,21 +7,28 @@ import org.dcsa.core.events.model.enums.OperationsEventTypeCode;
 import org.dcsa.core.events.model.enums.PortCallServiceTypeCode;
 import org.dcsa.core.events.model.transferobjects.LocationTO;
 import org.dcsa.core.events.model.transferobjects.PartyTO;
+import org.dcsa.core.util.ValidationUtils;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.OffsetDateTime;
 
 @Data
 @Table("timestamp")
 public class Timestamp {
+
+    @Size(max = 6)
     private String facilitySMDGCode;
 
     @NotNull
+    @Size(min = 4, max = 4)
+    // TODO Move FacilityTypeCode enum out of EBL and into Event-Core
     private String facilityTypeCode;
 
     @NotNull
+    @Size(max = 5)
     @JsonProperty("UNLocationCode")
     private String UNLocationCode;
 
@@ -29,15 +36,23 @@ public class Timestamp {
     private String publisherRole;
 
     @NotNull
+    @Size(min = 7, max = 7)
     private String vesselIMONumber;
 
+    public void setVesselIMONumber(String vesselIMONumber) {
+        ValidationUtils.validateVesselIMONumber(vesselIMONumber);
+        this.vesselIMONumber = vesselIMONumber;
+    }
+
+    @Size(max = 3)
     private String modeOfTransport;
+
     @NotNull
     private EventClassifierCode eventClassifierCode;
 
     @NotNull
     private OffsetDateTime eventDateTime;
-    
+
     @NotNull
     private OperationsEventTypeCode operationsEventTypeCode;
 
