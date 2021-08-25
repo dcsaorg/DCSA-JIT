3d7a1a672b5f77a41bbc89b228e710d7645dd368
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-08-25 13:40:06 +0200

    Refactored code and Removed unofficial endpoints.

---
+++ src/main/java/org/dcsa/ovs/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package org.dcsa.ovs.config;
+
+import org.dcsa.core.repository.ExtendedRepositoryImpl;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@ComponentScan("org.dcsa")
+@EnableR2dbcRepositories(
+    basePackages = {"org.dcsa"},
+    repositoryBaseClass = ExtendedRepositoryImpl.class)
+@EnableScheduling
+public class ApplicationConfig {}
