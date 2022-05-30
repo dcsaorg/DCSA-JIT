75c6a1efeff19534d7d95d9ecb2152748e05298e
Niels Thykier <nt@asseco.dk>
2022-05-30 14:31:06 +0200

    Bump spec and include a basic Integration test
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ jit-integration-tests/src/test/java/org/dcsa/jit/itests/config/RestAssuredConfigurator.java
@@ -0,0 +1,13 @@
+package org.dcsa.jit.itests.config;
+
+import io.restassured.RestAssured;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RestAssuredConfigurator {
+  public void initialize() {
+    var properties = IntegrationTestsProperties.getInstance();
+    RestAssured.baseURI = properties.getBaseUri();
+    RestAssured.port = properties.getPort();
+  }
+}
