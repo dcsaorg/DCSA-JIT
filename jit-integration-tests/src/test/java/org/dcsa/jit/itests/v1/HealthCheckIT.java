2cfc6d40f8d26f27a7b5c7430cba94dda3b541fd
Niels Thykier <nt@asseco.dk>
2022-05-30 14:31:06 +0200

    Bump spec and include a basic Integration test
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ jit-integration-tests/src/test/java/org/dcsa/jit/itests/v1/HealthCheckIT.java
@@ -0,0 +1,29 @@
+package org.dcsa.jit.itests.v1;
+
+import io.restassured.http.ContentType;
+import org.dcsa.jit.itests.config.RestAssuredConfigurator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HealthCheckIT {
+  @BeforeAll
+  public static void initializeRestAssured() {
+    RestAssuredConfigurator.initialize();
+  }
+
+  @Test
+  public void testHealth() {
+    given()
+      .contentType("application/json")
+      .get("/v1/actuator/health")
+      .then()
+      .assertThat()
+      .statusCode(200)
+      .contentType(ContentType.JSON)
+      .body("status", equalTo("UP"))
+    ;
+  }
+}
