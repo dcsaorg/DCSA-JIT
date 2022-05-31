7bdd1217db5f3f358cc97f7104faf275cc9285da
Martin Petersson <mp@asseco.dk>
2022-05-31 14:06:50 +0200

    Update PartyTO.java

--- jit-transfer-obj/src/main/java/org/dcsa/jit/transferobjects/PartyTO.java
+++ jit-transfer-obj/src/main/java/org/dcsa/jit/transferobjects/PartyTO.java
@@ -1,40 +1,23 @@
 package org.dcsa.jit.transferobjects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.dcsa.skernel.domain.persistence.entity.Address;
+import org.dcsa.skernel.domain.persistence.entity.enums.FacilityCodeListProvider;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
-/**
- * NOTE: This Party is specific to JIT and should not be added to Shared-Kernel.
- */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter(AccessLevel.PRIVATE)
-@Entity
-@Table(name = "party")
-public class PartyTO {
-  @Id
-  @GeneratedValue
-  @Column(name = "id", nullable = false)
-  private UUID id;
-
-  @Column(name = "party_name", length = 100)
-  private String name;
-
-  @Column(name = "tax_reference_1", length = 20)
-  private String taxReference1;
-
-  @Column(name = "tax_reference_2", length = 20)
-  private String taxReference2;
-
-  @Column(name = "public_key", length = 100)
-  private String publicKey;
-
-  // TODO fix this reference
-  // @ManyToOne(fetch = FetchType.LAZY)
-  // @JoinColumn(name = "address_id")
-  // private Address address;
+public record PartyTO(
+  UUID id,
+  @Size(max = 100) String name,
+  @Size(max = 20) String taxReference1,
+  @Size(max = 20) String taxReference2,
+  @Size(max = 500) String publicKey,
+  Address address
+//  List<IdentifyingCode> identifyingCodes;
+) {
+  @Builder // workaround for intellij issue
+  public PartyTO { }
 }
