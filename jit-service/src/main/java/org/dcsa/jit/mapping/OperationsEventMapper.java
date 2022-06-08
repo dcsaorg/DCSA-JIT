4dc0e0f9074febbb0548b9c59895c4448812a0fd
Martin Petersson <mp@asseco.dk>
2022-06-08 11:31:02 +0200

    Update OperationsEventMapper.java

--- jit-service/src/main/java/org/dcsa/jit/mapping/OperationsEventMapper.java
+++ jit-service/src/main/java/org/dcsa/jit/mapping/OperationsEventMapper.java
@@ -1,9 +1,7 @@
 package org.dcsa.jit.mapping;
 
 import org.dcsa.jit.persistence.entity.OperationsEvent;
-import org.dcsa.jit.transferobjects.LocationTO;
 import org.dcsa.jit.transferobjects.OperationsEventTO;
-import org.dcsa.skernel.domain.persistence.entity.Location;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
