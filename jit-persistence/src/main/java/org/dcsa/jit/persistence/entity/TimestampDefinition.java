8b5a3c0095508452e63e87341bdfce350e2f2388
Finn Nielsen <fn@asseco.dk>
2022-06-13 10:09:01 +0200

    DDT-1104-publish-jit-artifacts-3

--- jit-persistence/src/main/java/org/dcsa/jit/persistence/entity/TimestampDefinition.java
+++ jit-persistence/src/main/java/org/dcsa/jit/persistence/entity/TimestampDefinition.java
@@ -26,66 +26,64 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Setter(AccessLevel.PRIVATE)
 @Entity
-@Table(name="timestamp_definition")
+@Table(name = "timestamp_definition")
 public class TimestampDefinition {
+  @Id
+  @Column(name = "id", nullable = false)
+  private String id;
 
-    @Id
-    @Column(name="id", nullable = false)
-    private String id;
+  @Column(name = "timestamp_type_name", nullable = false, unique = true)
+  private String timestampTypeName;
 
-    @Column(name="timestamp_type_name")
-    private String timestampTypeName;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "publisher_role", length = 3, nullable = false)
+  private PublisherRole publisherRole;
 
-    @Column(name="publisher_role")
-    @Enumerated(EnumType.STRING)
-    private PublisherRole publisherRole;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "primary_receiver", length = 3, nullable = false)
+  private PublisherRole primaryReceiver;
 
-    @Column(name="primary_receiver")
-    @Enumerated(EnumType.STRING)
-    private PublisherRole primaryReceiver;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "event_classifier_code", length = 3, nullable = false)
+  private EventClassifierCode eventClassifierCode;
 
-    @Column(name="event_classifier_code")
-    @Enumerated(EnumType.STRING)
-    private EventClassifierCode eventClassifierCode;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "operations_event_type_code", length = 4, nullable = false)
+  private OperationsEventTypeCode operationsEventTypeCode;
 
-    @Column(name="operations_event_type_code")
-    @Enumerated(EnumType.STRING)
-    private OperationsEventTypeCode operationsEventTypeCode;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "port_call_phase_type_code", length = 4)
+  private PortCallPhaseTypeCode portCallPhaseTypeCode;
 
-    @Column(name="port_call_phase_type_code")
-    @Enumerated(EnumType.STRING)
-    private PortCallPhaseTypeCode portCallPhaseTypeCode;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "port_call_service_type_code", length = 4)
+  private PortCallServiceTypeCode portCallServiceTypeCode;
 
-    @Column(name="port_call_service_type_code")
-    @Enumerated(EnumType.STRING)
-    private PortCallServiceTypeCode portCallServiceTypeCode;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "facility_type_code", length = 4)
+  private FacilityTypeCode facilityTypeCode;
 
-    @Column(name="facility_type_code")
-    @Enumerated(EnumType.STRING)
-    private FacilityTypeCode facilityTypeCode;
+  @Column(name = "is_berth_location_needed", nullable = false)
+  private Boolean isBerthLocationNeeded;
 
-    @Column(name="is_berth_location_needed")
-    private Boolean isBerthLocationNeeded;
+  @Column(name = "is_pbp_location_needed", nullable = false)
+  private Boolean isPBPLocationNeeded;
 
-    @Column(name="is_pbp_location_needed")
-    private Boolean isPBPLocationNeeded;
+  @Column(name = "is_terminal_needed", nullable = false)
+  private Boolean isTerminalNeeded;
 
-    @Column(name="is_terminal_needed")
-    private Boolean isTerminalNeeded;
+  @Column(name = "is_vessel_position_needed", nullable = false)
+  private Boolean isVesselPositionNeeded;
 
-    @Column(name="is_vessel_position_needed")
-    private Boolean isVesselPositionNeeded;
+  @Column(name = "negotiation_cycle", nullable = false)
+  private String negotiationCycle;
 
-    @Column(name="negotiation_cycle")
-    private String negotiationCycle;
+  @Column(name = "provided_in_standard", nullable = false)
+  private String providedInStandard;
 
-    @Column(name="provided_in_standard")
-    private String providedInStandard;
+  @Column(name = "accept_timestamp_definition", nullable = false)
+  private String acceptTimestampDefinition;
 
-    @Column(name="accept_timestamp_definition")
-    private String acceptTimestampDefinition;
-
-    @Column(name="reject_timestamp_definition")
-    private String rejectTimestampDefinition;
-
+  @Column(name = "reject_timestamp_definition", nullable = false)
+  private String rejectTimestampDefinition;
 }
