d5002aa6acaffa068a29e6df128c7050bf02dadc
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2022-06-16 11:48:16 +0200

    Removed Persistable as it is not required.

--- jit-persistence/src/main/java/org/dcsa/jit/persistence/entity/Vessel.java
+++ jit-persistence/src/main/java/org/dcsa/jit/persistence/entity/Vessel.java
@@ -1,21 +1,8 @@
 package org.dcsa.jit.persistence.entity;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.domain.Persistable;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
@@ -25,7 +12,7 @@ import java.util.UUID;
 @Setter(AccessLevel.PRIVATE)
 @Entity
 @Table(name = "vessel")
-public class Vessel implements Persistable<UUID> {
+public class Vessel {
   @Id
   @GeneratedValue
   @Column(name = "id", nullable = false)
@@ -37,7 +24,10 @@ public class Vessel implements Persistable<UUID> {
   @Column(name = "vessel_name", length = 35)
   private String vesselName;
 
-  @Column(name = "vessel_flag", length = 2, columnDefinition = "bpchar") // "bpchar" here is not a typing error)
+  @Column(
+      name = "vessel_flag",
+      length = 2,
+      columnDefinition = "bpchar") // "bpchar" here is not a typing error)
   private String vesselFlag;
 
   @Column(name = "vessel_call_sign", length = 18)
@@ -58,9 +48,4 @@ public class Vessel implements Persistable<UUID> {
 
   @Column(name = "dimension_unit", length = 3)
   private String dimensionUnit;
-
-  @Override
-  public boolean isNew() {
-    return this.id == null;
-  }
 }
