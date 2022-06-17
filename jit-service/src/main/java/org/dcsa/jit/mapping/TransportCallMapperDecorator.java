bd84682e8868cc0d95b19bd5779e14f9bf489cd0
Finn Nielsen <fn@asseco.dk>
2022-06-17 14:15:32 +0200

    DDT-1103 - post and get transport-calls

---
+++ jit-service/src/main/java/org/dcsa/jit/mapping/TransportCallMapperDecorator.java
@@ -0,0 +1,42 @@
+package org.dcsa.jit.mapping;
+
+import org.dcsa.jit.persistence.entity.TransportCall;
+import org.dcsa.jit.transferobjects.TransportCallTO;
+import org.dcsa.jit.transferobjects.enums.FacilityCodeListProvider;
+import org.dcsa.skernel.domain.persistence.entity.Facility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public abstract class TransportCallMapperDecorator extends TransportCallMapper {
+  // NOTE constructor injection doesn't work here, mapstruct would generate class with no constructor
+  @Autowired
+  @Qualifier("delegate")
+  private TransportCallMapper delegate;
+
+  @Override
+  public TransportCallTO toTO(TransportCall transportCall) {
+    Facility facility = null;
+    if (transportCall.getLocation() != null) {
+      facility = transportCall.getLocation().getFacility();
+    }
+    if (facility == null) {
+      // Fallback to deprecated way for now because location.facility is not always populated in our test data
+      facility = transportCall.getFacility();
+    }
+    if (facility != null) {
+      TransportCallTO.TransportCallTOBuilder builder = delegate.toTO(transportCall).toBuilder();
+      if (facility.getFacilitySMDGCode() != null) {
+        builder.facilityCode(facility.getFacilitySMDGCode());
+        builder.facilityCodeListProvider(FacilityCodeListProvider.SMDG);
+      } else if (facility.getFacilityBICCode() != null) {
+        builder.facilityCode(facility.getFacilityBICCode());
+        builder.facilityCodeListProvider(FacilityCodeListProvider.BIC);
+      } else {
+        throw new IllegalArgumentException("Unsupported facility code list provider.");
+      }
+      return builder.build();
+    }
+
+    return delegate.toTO(transportCall);
+  }
+}
---
+++ jit-service/src/main/java/org/dcsa/jit/service/VesselService.java
@@ -0,0 +1,30 @@
+package org.dcsa.jit.service;
+
+import lombok.RequiredArgsConstructor;
+import org.dcsa.jit.persistence.entity.Vessel;
+import org.dcsa.jit.persistence.repository.VesselRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class VesselService {
+  private final VesselRepository vesselRepository;
+
+  @Transactional
+  public Vessel ensureVesselExistsByImoNumber(String imoNumber) {
+    return ensureVesselExistsByImoNumber(imoNumber, false);
+  }
+
+  @Transactional
+  public Vessel ensureVesselExistsByImoNumber(String imoNumber, boolean dummy) {
+    return vesselRepository.findByVesselIMONumber(imoNumber)
+      .orElseGet(() -> vesselRepository.save(
+        Vessel.builder()
+          .vesselIMONumber(imoNumber)
+          .isDummy(dummy)
+          .build()
+      ));
+  }
+}
