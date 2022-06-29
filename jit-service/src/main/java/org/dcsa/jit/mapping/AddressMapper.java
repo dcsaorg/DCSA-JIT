8f4cbb457397aeaa865c2410a30e11d98801096f
Martin Petersson <mp@asseco.dk>
2022-06-29 15:59:51 +0200

    Update AddressMapper.java

--- jit-service/src/main/java/org/dcsa/jit/mapping/AddressMapper.java
+++ jit-service/src/main/java/org/dcsa/jit/mapping/AddressMapper.java
@@ -3,14 +3,11 @@ package org.dcsa.jit.mapping;
 import org.dcsa.jit.transferobjects.AddressTO;
 import org.dcsa.skernel.domain.persistence.entity.Address;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
 
-  @Mapping(target = "postalCode", source = "postCode")
   Address toDao(AddressTO addressTO);
 
-  @Mapping(target = "postCode", source = "postalCode")
   AddressTO toDTO(Address address);
 }
