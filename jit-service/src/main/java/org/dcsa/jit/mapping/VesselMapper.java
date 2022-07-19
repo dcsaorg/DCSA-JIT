ca65080c56f2f786c2c31f6f36742047e14f2937
abf <abf@asseco.dk>
2022-07-19 13:00:00 +0200

    Add mapping for vesselOperatorCarrier

--- jit-service/src/main/java/org/dcsa/jit/mapping/VesselMapper.java
+++ jit-service/src/main/java/org/dcsa/jit/mapping/VesselMapper.java
@@ -2,14 +2,30 @@ package org.dcsa.jit.mapping;
 
 import org.dcsa.jit.persistence.entity.Vessel;
 import org.dcsa.jit.transferobjects.VesselTO;
+import org.dcsa.jit.transferobjects.enums.CarrierCodeListProvider;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface VesselMapper {
-  // FIXME @Mapping(target = "vesselOperatorCarrierCode", source = "vesselOperatorCarrier.") // smdgCode or nmftaCode
   VesselTO toTO(Vessel vessel);
 
   @Mapping(target = "isDummy", source = "isDummy", defaultValue = "false")
   Vessel toEntity(VesselTO vesselTO);
+
+  @AfterMapping
+  default void mapVesselOperatorCarrier(
+      Vessel vessel, @MappingTarget VesselTO.VesselTOBuilder vesselTOBuilder) {
+    String nMFTACode = vessel.getVesselOperatorCarrier().getNmftaCode();
+    String sMDGCode = vessel.getVesselOperatorCarrier().getSmdgCode();
+    if (nMFTACode != null) {
+      vesselTOBuilder.vesselOperatorCarrierCodeListProvider(CarrierCodeListProvider.NMFTA);
+      vesselTOBuilder.vesselOperatorCarrierCode(vessel.getVesselOperatorCarrier().getNmftaCode());
+    } else if (sMDGCode != null) {
+      vesselTOBuilder.vesselOperatorCarrierCodeListProvider(CarrierCodeListProvider.SMDG);
+      vesselTOBuilder.vesselOperatorCarrierCode(vessel.getVesselOperatorCarrier().getSmdgCode());
+    }
+  }
 }
