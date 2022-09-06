fe9bfb8162a5f402103d7890d20c600bacb8b5f1
Niels Thykier <nt@asseco.dk>
2022-09-06 14:53:33 +0200

    Do not allow empty party codes
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- jit-transfer-obj/src/main/java/org/dcsa/jit/transferobjects/IdentifyingCodeTO.java
+++ jit-transfer-obj/src/main/java/org/dcsa/jit/transferobjects/IdentifyingCodeTO.java
@@ -3,13 +3,14 @@ package org.dcsa.jit.transferobjects;
 import lombok.Builder;
 import org.dcsa.jit.transferobjects.enums.DCSAResponsibleAgencyCode;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public record IdentifyingCodeTO(
   @Size(max = 3) String codeListResponsibleAgencyCode,
   @NotNull DCSAResponsibleAgencyCode DCSAResponsibleAgencyCode,
-  @NotNull @Size(max = 100) String partyCode,
+  @NotEmpty @Size(max = 100) String partyCode,
   @Size(max = 100) String codeListName
 ) {
   @Builder(toBuilder = true) // workaround for intellij issue
