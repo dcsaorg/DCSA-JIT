0800b776fb24928be373f80f70a4d508240d01b5
Niels Thykier <nt@asseco.dk>
2022-11-08 13:32:17 +0100

    DDT-1290: Remove unused UnmappedEvent entity
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- jit-persistence/src/main/java/org/dcsa/jit/persistence/repository/UnmappedEventRepository.java
+++
@@ -1,8 +0,0 @@
-package org.dcsa.jit.persistence.repository;
-
-import org.dcsa.jit.persistence.entity.UnmappedEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UnmappedEventRepository extends JpaRepository<UnmappedEvent, UUID> {}
